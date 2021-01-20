ddc8eaf46ff1cc0ea82d811689e38c797448796b
leang27 <hangleang127@gmail.com>
2021-01-20 14:40:24 +0700

    change mod path

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
-mod helpers;
-mod session;
-
-// pub use network_manager::*;
-pub use session::PowerManager;
-
-#[cfg(test)]
-mod tests {
-
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+pub mod helpers;
+pub mod session;
+pub mod system_settings;
