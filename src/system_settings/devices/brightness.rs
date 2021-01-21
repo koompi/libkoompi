c05a9bb2b6fda6f1bf3fb7f6dd79a8bd4d4a63ac
veasnama <maveasnalearntosolve@gmail>
2021-01-21 16:33:24 +0700

    set brigthness via login1 dbus

--- src/system_settings/devices/brightness.rs
+++ src/system_settings/devices/brightness.rs
@@ -1,12 +1,35 @@
 #![allow(dead_code)]
 use crate::helpers::{get_property, set_device_path};
-use udev::Enumerator;
+use dbus::blocking::Connection;
 pub struct Brightness {
     device: BDevice,
 }
 
 impl Brightness {
-    pub fn set_current(&mut self) {}
+    pub fn new() -> Self {
+        Self {
+            device: BDevice::new(),
+        }
+    }
+    pub fn set_percent(&mut self, percent: u32) {
+        self.device.set_bright(percent);
+    }
+    pub fn get_percent(&self) -> u32 {
+        self.device.get_current_level()
+    }
+    pub fn get_max_percent(&self) -> u32 {
+        self.device.get_max_bright()
+    }
+    pub fn login1_set_brightness(&self, level: u32) -> Result<(), Box<dyn std::error::Error>> {
+        match self.device.set_dbus_bright(level) {
+            Ok(()) => println!("run fine"),
+            Err(e) => println!("Not crash but lovable error: {:?}", e),
+        }
+        Ok(())
+    }
+    pub fn information(&self) -> Vec<u32> {
+        self.device.info()
+    }
 }
 #[derive(Default)]
 struct BDevice {
@@ -15,7 +38,6 @@ struct BDevice {
     max_brightness: u32,
     current_brightness: u32,
 }
-
 impl BDevice {
     fn new() -> Self {
         let max_bright = get_property("max_brightness");
@@ -27,7 +49,7 @@ impl BDevice {
         }
     }
     /// set brightness for dispaly device
-    pub fn set_bright(&mut self, level: u32) {
+    fn set_bright(&mut self, level: u32) {
         match set_device_path("backlight/intel_backlight") {
             Ok(mut dev) => {
                 if dev.is_initialized() {
@@ -35,10 +57,10 @@ impl BDevice {
                 }
                 // give time for cpu to execute on other processes
                 std::thread::sleep(std::time::Duration::from_millis(10));
-                match dev.set_attribute_value("brightness", level.to_string()) {
+                let value = self.percent_to_value(level);
+                match dev.set_attribute_value("brightness", value.to_string()) {
                     Ok(()) => {
-                        self.update(level);
-                        println!("value to percent: {:?}", self.val_to_percent(level, true));
+                        self.update(value);
                     }
                     Err(e) => {
                         println!("Error: {:?}", e);
@@ -50,7 +72,7 @@ impl BDevice {
     }
     // return roundf(powf(percent / 100, p.exponent) * d->max_brightness);
     // calculate percent value range 0..=100 by by round_up(f32::powf(input_v / 100, 1) * max_value, -2) as u32
-    pub fn percent_to_value(&self, val: u32) -> u32 {
+    fn percent_to_value(&self, val: u32) -> u32 {
         math::round::ceil(
             (f32::powf(val as f32 / 100.0, 1.0) * self.get_max_bright() as f32) as f64,
             -2,
@@ -72,34 +94,78 @@ impl BDevice {
             }
         }
     }
-    pub fn get_max_bright(&self) -> u32 {
+    fn get_max_bright(&self) -> u32 {
         self.max_brightness
     }
-
-    pub fn info(&self) -> Vec<u32> {
-        vec![self.max_brightness, self.current_bright()]
+    // bool logind_set_brightness(struct device *d) {
+    //     sd_bus *bus = NULL;
+    //     int r = sd_bus_default_system(&bus);
+    //     if (r < 0) {
+    //         fprintf(stderr, "Can't connect to system bus: %s\n", strerror(-r));
+    //         return false;
+    //     }
+    //     r = sd_bus_call_method(bus,
+    //                    "org.freedesktop.login1",
+    //                    "/org/freedesktop/login1/session/auto",
+    //                    "org.freedesktop.login1.Session",
+    //                    "SetBrightness",
+    //                    NULL,
+    //                    NULL,
+    //                    "ssu",
+    //                    d->class,
+    //                    d->id,
+    //                    d->curr_brightness);
+    //     if (r < 0)
+    //         fprintf(stderr, "Failed to set brightness: %s\n", strerror(-r));
+    //     sd_bus_unref(bus);
+    //     return r >= 0;
+    // }
+    fn set_dbus_bright(&self, level: u32) -> Result<(), Box<dyn std::error::Error>> {
+        let conn = Connection::new_system()?;
+        let proxy = conn.with_proxy(
+            "org.freedesktop.login1",
+            "/org/freedesktop/login1/session/auto",
+            std::time::Duration::from_millis(100),
+        );
+        let (status,): (bool,) = proxy.method_call(
+            "org.freedesktop.login1.Session",
+            "SetBrightness",
+            ("backlight", "intel_backlight", level),
+        )?;
+        println!("dubs stats: {:?}", status);
+        if status {
+            println!("set brigthness success");
+        } else {
+            println!("failed to set bright nesss");
+        }
+        Ok(())
+    }
+    fn get_current_level(&self) -> u32 {
+        self.val_to_percent(self.current_brightness, true)
+    }
+    fn info(&self) -> Vec<u32> {
+        vec![self.max_brightness, self.get_current_level()]
     }
     fn update(&mut self, current: u32) {
         self.current_brightness = current;
-    }
-    pub fn current_bright(&self) -> u32 {
-        self.val_to_percent(self.current_brightness, true)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::BDevice;
+    use super::Brightness;
+    // use std::time::Duration;
     #[test]
     fn it_works() {
-        let mut bright = BDevice::new();
-        println!("current : {}", bright.current_bright());
-        assert_eq!(100, bright.current_bright());
-        assert_eq!(24000, bright.percent_to_value(100));
-        for i in 1..=10 {
-            std::thread::sleep(std::time::Duration::from_millis(1000));
-            bright.set_bright(2400 * i);
-        }
-        assert_eq!(2 + 2, 4);
+        let bright = Brightness::new();
+        bright.login1_set_brightness(15000).unwrap();
+        // println!("current : {}", bright.get_percent());
+        // println!("max_percent: {}", bright.get_max_percent());
+        // for i in 1..=10 {
+        //     std::thread::sleep(Duration::from_millis(1000));
+        //     bright.set_percent(i * 10);
+        // }
+        // assert_eq!(100, bright.get_percent());
+        assert_eq!(2 + 2, 3);
     }
 }
--- src/system_settings/devices/hardware.rs
+++ src/system_settings/devices/hardware.rs
@@ -4,39 +4,12 @@ use udev::{Devices, Enumerator, Hwdb};
 mod tests {
     #[test]
     fn it_works() {
-        let hardware = udev::Hwdb::new();
-        match hardware {
-            Ok(h_context) => {}
-            Err(e) => {}
-        }
+        // let device = udev::Device::from_syspath(std::path::Path::new("/sys/class/bluetooth"));
+        // device.iter().enumerate().for_each(|(key, value)| {
+        //     println!("{:?} : {:?}", key, value.sysname());
+        // });
+        let enum_dev = udev::Enumerator::new().unwrap();
 
-        let mut enumerator = udev::Enumerator::new().unwrap();
-        for device in enumerator.scan_devices().unwrap() {
-            println!();
-            println!("initialized: {:?}", device.is_initialized());
-            println!("     devnum: {:?}", device.devnum());
-            println!("    syspath: {:?}", device.syspath());
-            println!("    devpath: {:?}", device.devpath());
-            println!("  subsystem: {:?}", device.subsystem());
-            println!("    sysname: {:?}", device.sysname());
-            println!("     sysnum: {:?}", device.sysnum());
-            println!("    devtype: {:?}", device.devtype());
-            println!("     driver: {:?}", device.driver());
-            println!("    devnode: {:?}", device.devnode());
-            if let Some(parent) = device.parent() {
-                println!("     parent: {:?}", parent.syspath());
-            } else {
-                println!("     parent: None");
-            }
-            println!("  [properties]");
-            for property in device.properties() {
-                println!("    - {:?} {:?}", property.name(), property.value());
-            }
-            println!("  [attributes]");
-            for attribute in device.attributes() {
-                println!("    - {:?} {:?}", attribute.name(), attribute.value());
-            }
-        }
         assert_eq!(2 + 2, 3);
     }
 }
