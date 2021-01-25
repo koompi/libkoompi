967fcbde77f5c989083b75284efdcd54b9227c5f
veasnama <maveasnalearntosolve@gmail>
2021-01-25 20:31:40 +0700

    update nm dbus up-to-date still futures and tokio-timer.

--- src/system_settings/network/types.rs
+++ src/system_settings/network/types.rs
@@ -1,3 +1,5 @@
+use super::errors::*;
+use std::str::FromStr;
 #[derive(Debug, Clone, Eq, PartialEq)]
 pub enum ConnectionState {
     Unknown = 0,
@@ -7,6 +9,21 @@ pub enum ConnectionState {
     Deactivated = 4,
 }
 
+impl From<i64> for ConnectionState {
+    fn from(state: i64) -> Self {
+        match state {
+            0 => ConnectionState::Unknown,
+            1 => ConnectionState::Activating,
+            2 => ConnectionState::Activated,
+            3 => ConnectionState::Deactivating,
+            4 => ConnectionState::Deactivated,
+            _ => {
+                log::warn!("Undefined connection state: {}", state);
+                ConnectionState::Unknown
+            }
+        }
+    }
+}
 #[derive(Clone, Debug, PartialEq)]
 pub enum DeviceType {
     Unknown,
@@ -33,6 +50,40 @@ pub enum DeviceType {
     Macsec,
     Dummy,
 }
+impl From<i64> for DeviceType {
+    fn from(device_type: i64) -> Self {
+        match device_type {
+            0 => DeviceType::Unknown,
+            1 => DeviceType::Ethernet,
+            2 => DeviceType::WiFi,
+            3 => DeviceType::Unused1,
+            4 => DeviceType::Unused2,
+            5 => DeviceType::Bt,
+            6 => DeviceType::OlpcMesh,
+            7 => DeviceType::Wimax,
+            8 => DeviceType::Modem,
+            9 => DeviceType::Infiniband,
+            10 => DeviceType::Bond,
+            11 => DeviceType::Vlan,
+            12 => DeviceType::Adsl,
+            13 => DeviceType::Bridge,
+            14 => DeviceType::Generic,
+            15 => DeviceType::Team,
+            16 => DeviceType::Tun,
+            17 => DeviceType::IpTunnel,
+            18 => DeviceType::Macvlan,
+            19 => DeviceType::Vxlan,
+            20 => DeviceType::Veth,
+            21 => DeviceType::Macsec,
+            22 => DeviceType::Dummy,
+            _ => {
+                log::warn!("Undefined device type: {}", device_type);
+                DeviceType::Unknown
+            }
+        }
+    }
+}
+
 #[derive(Clone, Debug, PartialEq)]
 pub enum DeviceState {
     Unknown,
@@ -49,6 +100,63 @@ pub enum DeviceState {
     Deactivating,
     Failed,
 }
+impl From<i64> for DeviceState {
+    fn from(state: i64) -> Self {
+        match state {
+            0 => DeviceState::Unknown,
+            10 => DeviceState::Unmanaged,
+            20 => DeviceState::Unavailable,
+            30 => DeviceState::Disconnected,
+            40 => DeviceState::Prepare,
+            50 => DeviceState::Config,
+            60 => DeviceState::NeedAuth,
+            70 => DeviceState::IpConfig,
+            80 => DeviceState::IpCheck,
+            90 => DeviceState::Secondaries,
+            100 => DeviceState::Activated,
+            110 => DeviceState::Deactivating,
+            120 => DeviceState::Failed,
+            _ => {
+                log::warn!("Undefined device state: {}", state);
+                DeviceState::Unknown
+            }
+        }
+    }
+}
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Connectivity {
+    Unknown,
+    None,
+    Portal,
+    Limited,
+    Full,
+}
+
+impl From<u32> for Connectivity {
+    fn from(state: u32) -> Self {
+        match state {
+            0 => Connectivity::Unknown,
+            1 => Connectivity::None,
+            2 => Connectivity::Portal,
+            3 => Connectivity::Limited,
+            4 => Connectivity::Full,
+            _ => {
+                log::warn!("Undefined connectivity state: {}", state);
+                Connectivity::Unknown
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum ActiveConnectionState {
+    Unknown = 0,
+    Activating = 1,
+    Activated = 2,
+    Deactivating = 3,
+    Deactivated = 4,
+}
 
 #[derive(Clone, Debug, PartialEq)]
 pub enum NetworkManagerState {
@@ -61,15 +169,25 @@ pub enum NetworkManagerState {
     ConnectedSite,
     ConnectedGlobal,
 }
-
-#[derive(Clone, Debug, PartialEq)]
-pub enum Connectivity {
-    Unknown,
-    None,
-    Portal,
-    Limited,
-    Full,
+impl From<u32> for NetworkManagerState {
+    fn from(state: u32) -> Self {
+        match state {
+            0 => NetworkManagerState::Unknown,
+            10 => NetworkManagerState::Asleep,
+            20 => NetworkManagerState::Disconnected,
+            30 => NetworkManagerState::Disconnecting,
+            40 => NetworkManagerState::Connecting,
+            50 => NetworkManagerState::ConnectedLocal,
+            60 => NetworkManagerState::ConnectedSite,
+            70 => NetworkManagerState::ConnectedGlobal,
+            _ => {
+                log::warn!("Undefined Network Manager state: {}", state);
+                NetworkManagerState::Unknown
+            }
+        }
+    }
 }
+
 #[derive(Debug, Eq, PartialEq)]
 pub enum ServiceState {
     Active,
@@ -80,11 +198,17 @@ pub enum ServiceState {
     Deactivating,
 }
 
-#[derive(Debug)]
-pub enum ActiveConnectionState {
-    Unknown = 0,
-    Activating = 1,
-    Activated = 2,
-    Deactivating = 3,
-    Deactivated = 4,
+impl FromStr for ServiceState {
+    type Err = Error;
+    fn from_str(s: &str) -> Result<ServiceState> {
+        match s {
+            "active" => Ok(ServiceState::Active),
+            "reloading" => Ok(ServiceState::Reloading),
+            "inactive" => Ok(ServiceState::Inactive),
+            "failed" => Ok(ServiceState::Failed),
+            "activating" => Ok(ServiceState::Activating),
+            "deactivating" => Ok(ServiceState::Deactivating),
+            _ => Err(ErrorKind::Service.into()),
+        }
+    }
 }
