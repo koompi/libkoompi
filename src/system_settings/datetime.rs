af53e3180b0624cc3e314f0b6be4f8cb52b4ba5a
leang27 <hangleang127@gmail.com>
2021-02-02 11:05:17 +0700

    datetime manager list timezones by continent

--- src/system_settings/datetime.rs
+++ src/system_settings/datetime.rs
@@ -1,38 +1,40 @@
 use std::io::Error;
 use crate::helpers::{get_bool_yesno, exec_cmd};
 use getset::{Getters};
+use std::collections::HashMap;
+use itertools::Itertools;
 
 const TIMEDATE_CTL: &'static str = "timedatectl";
 
 #[derive(Debug, Clone, Getters)]
 pub struct DateTimeManager {
-    #[getset(get = "pub")]
-    timezone: String,
-    local_rtc: bool,
-    can_ntp: bool,
-    #[getset(get = "pub")]
-    ntp: bool,
-    ntp_sync: bool,
-    #[getset(get = "pub")]
-    time_usec: String,
-    rtc_time_usec: String,
-    #[getset(get = "pub")]
-    list_timezones: Vec<String>,
+   #[getset(get = "pub")]
+   timezone: String,
+   local_rtc: bool,
+   can_ntp: bool,
+   #[getset(get = "pub")]
+   ntp: bool,
+   ntp_sync: bool,
+   #[getset(get = "pub")]
+   time_usec: String,
+   rtc_time_usec: String,
+   #[getset(get = "pub")]
+   list_timezones: HashMap<String, Vec<String>>,
 }
 
 impl Default for DateTimeManager {
-    fn default() -> Self {
-        Self {
-            timezone: String::default(),
-            local_rtc: false,
-            can_ntp: true,
-            ntp: true,
-            ntp_sync: true,
-            time_usec: String::default(),
-            rtc_time_usec: String::default(),
-            list_timezones: Vec::new(),
-        }
-    }
+   fn default() -> Self {
+      Self {
+         timezone: String::default(),
+         local_rtc: false,
+         can_ntp: true,
+         ntp: true,
+         ntp_sync: true,
+         time_usec: String::default(),
+         rtc_time_usec: String::default(),
+         list_timezones: HashMap::new(),
+      }
+   }
 }
 
 impl DateTimeManager {
@@ -41,7 +43,10 @@ impl DateTimeManager {
       Self::load_info(&mut datetime_mn);
       match exec_cmd(TIMEDATE_CTL, vec!["list-timezones"]) {
          Ok(stdout) => {
-            datetime_mn.list_timezones = stdout.lines().map(|line| line.to_string()).collect();
+            let mut ls_timezones: Vec<String> = stdout.lines().map(|line| line.trim().to_string()).collect();
+            ls_timezones.push(String::from("Asia/Phnom_Penh"));
+            ls_timezones.sort();
+            datetime_mn.list_timezones = ls_timezones.into_iter().group_by(|tz| tz.split_terminator('/').collect::<Vec<&str>>().iter().map(ToString::to_string).collect::<Vec<String>>()[0].clone()).into_iter().map(|(con, cities)| (con.to_string(), cities.collect::<Vec<String>>())).collect();
          },
          Err(err) => eprintln!("{}", err), // error handling here
       }
@@ -171,7 +176,8 @@ mod tests {
                   println!("{}", *dt_mn.timezone());
                }
             } 
-            assert_eq!(*dt_mn.timezone(), "Asia/Phnom_Penh");
+            println!("{:#?}", dt_mn.list_timezones());
+            assert_eq!(*dt_mn.timezone(), "Phnom_Penh");
          },
          Err(err) => println!("{}", err)
       }
