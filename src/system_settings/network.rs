36b4347a783d46237b5d5dc6cd01b3d0a1036ecd
veasnama <maveasnalearntosolve@gmail>
2021-02-16 21:07:10 +0700

    add pub scope to network

--- src/system_settings/network.rs
+++ src/system_settings/network.rs
@@ -1,6 +1,6 @@
-mod accesspoint;
-mod auto_gen;
-mod wifi;
+pub mod accesspoint;
+pub mod auto_gen;
+pub mod wifi;
 pub use accesspoint::{get_accesspoints, AccessPoint};
 pub use auto_gen::*;
 pub use wifi::Wifi;
