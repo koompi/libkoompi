867c59b4874f6d83779dfbaf0c6362c04577433f
veasnama <maveasnalearntosolve@gmail>
2021-02-22 15:43:02 +0700

    catch errors && reduce current thread blockig execution

--- src/helpers.rs
+++ src/helpers.rs
@@ -1,8 +1,8 @@
 mod cmd_output_helper;
+pub mod constants;
 mod dbus_helper;
 mod device_file;
 mod file_helper;
-pub mod constants;
 
 pub use cmd_output_helper::*;
 pub use dbus_helper::*;
--- src/system_settings/network/accesspoint.rs
+++ src/system_settings/network/accesspoint.rs
@@ -22,7 +22,13 @@ pub struct AccessPoint {
 
 pub fn get_accesspoints() -> Result<Vec<AccessPoint>, Error> {
     let mut vec_accesspoint: Vec<AccessPoint> = Vec::new();
-    let conn = Connection::new_system()?;
+    let conn = match Connection::new_system() {
+        Ok(conn) => conn,
+        Err(e) => {
+            println!("Error: {:?}", e);
+            dbus::blocking::Connection::new_system()?
+        }
+    };
     let mut proxy = conn.with_proxy(SERVICE_NAME, "/org/freedesktop/NetworkManager", Duration::from_millis(2000));
     // Now we make the method call. The ListNames method call take zero input paramters and one output parameter which is an array of strings.Duration
     // Therefore the input is a zero tuple "()" , and the output is a single tuple "(names, "
@@ -37,29 +43,69 @@ pub fn get_accesspoints() -> Result<Vec<AccessPoint>, Error> {
     use dbus::blocking::stdintf::org_freedesktop_dbus::Properties;
     for i in &device_paths {
         proxy = conn.with_proxy(SERVICE_NAME, i, Duration::from_millis(500));
-        let dev_type: u32 = proxy.get("org.freedesktop.NetworkManager.Device", "DeviceType")?;
+        let dev_type: u32 = match proxy.get("org.freedesktop.NetworkManager.Device", "DeviceType") {
+            Ok(res) => res,
+            Err(e) => {
+                println!("Error: {:?}", e);
+                2
+            }
+        };
         if dev_type == 2 {
             let wifi_deice = Proxy::new(SERVICE_NAME, i, Duration::from_millis(500), &conn);
             let dict: PropMap = HashMap::new();
             let _device: Result<(), Error> = wifi_deice.method_call("org.freedesktop.NetworkManager.Device.Wireless", "RequestScan", (dict,));
-            std::thread::sleep(Duration::from_millis(1000));
+            std::thread::sleep(Duration::from_millis(500));
             let accessspoint: Result<Vec<dbus::Path<'static>>, dbus::Error> = wifi_deice.method_call("org.freedesktop.NetworkManager.Device.Wireless", "GetAccessPoints", ()).and_then(|r: (Vec<dbus::Path<'static>>,)| Ok(r.0));
-            accessspoint.unwrap().into_iter().for_each(|access_path| {
-                // println!("Path: {:?}", access_path);
-                let p = conn.with_proxy(SERVICE_INTERFACE, access_path, Duration::from_millis(2000));
-                let data: Result<Vec<u8>, dbus::Error> = p.get(ACESSPOINT_INTERFACE, "Ssid");
-                let address: Result<String, dbus::Error> = p.get(ACESSPOINT_INTERFACE, "HwAddress");
-                let strength: Result<u8, dbus::Error> = p.get(ACESSPOINT_INTERFACE, "Strength");
-                let result_strenght = u8::from_le_bytes([strength.unwrap(); 1]);
-                let longdata = &data.unwrap();
-                let acutal_data = std::str::from_utf8(longdata);
-                vec_accesspoint.push(AccessPoint {
-                    ssid: acutal_data.unwrap().to_string(),
-                    hwaddress: address.unwrap(),
-                    strenght: result_strenght,
-                    ..Default::default()
-                });
-            });
+            match accessspoint {
+                Ok(data) => {
+                    data.into_iter().for_each(|access_path| {
+                        // println!("Path: {:?}", access_path);
+                        let p = conn.with_proxy(SERVICE_INTERFACE, access_path, Duration::from_millis(500));
+                        let data: Result<Vec<u8>, dbus::Error> = p.get(ACESSPOINT_INTERFACE, "Ssid");
+                        let address: Result<String, dbus::Error> = p.get(ACESSPOINT_INTERFACE, "HwAddress");
+                        let strength: Result<u8, dbus::Error> = p.get(ACESSPOINT_INTERFACE, "Strength");
+                        let result_strenght = u8::from_le_bytes(
+                            [match strength {
+                                Ok(d) => d,
+                                Err(e) => {
+                                    println!("Error: {:?}", e);
+                                    // default signal when error
+                                    50
+                                }
+                            }; 1],
+                        );
+                        let longdata = match data {
+                            Ok(ssid_res) => ssid_res,
+                            Err(e) => {
+                                println!("Error: {:?}", e);
+                                Vec::new()
+                            }
+                        };
+                        let acutal_data = std::str::from_utf8(&longdata);
+                        vec_accesspoint.push(AccessPoint {
+                            ssid: match acutal_data {
+                                Ok(a_data) => a_data.to_string(),
+                                Err(e) => {
+                                    println!("Error: {:?}", e);
+                                    String::from("Unknown")
+                                }
+                            },
+                            hwaddress: match address {
+                                Ok(addr) => addr.to_string(),
+                                Err(e) => {
+                                    println!("Error: {:?}", e);
+                                    String::from("192.168.1.1")
+                                }
+                            },
+                            strenght: result_strenght,
+                            ..Default::default()
+                        });
+                    });
+                }
+                Err(e) => {
+                    println!("Error: {:?}", e);
+                }
+            }
         } else {
             continue;
         }
